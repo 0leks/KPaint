b9d63db21c5004cc328b78f2351bf4c7f34dca64
Oleks Korshak <alexokorshak@gmail.com>
2021-08-12 00:30:39 -0700

    multithread the edit history

--- src/ok/kpaint/history/History.java
+++ src/ok/kpaint/history/History.java
@@ -1,38 +1,56 @@
 package ok.kpaint.history;
 
-import java.awt.image.*;
 import java.util.*;
-
-import ok.kpaint.*;
+import java.util.concurrent.*;
 
 public class History {
-	
+	private static BlockingDeque<Edit> newEdits = new LinkedBlockingDeque<>();
 	private static LinkedList<Edit> edits = new LinkedList<>();
-	private static int stackPointer = 0;
+	private volatile static int stackPointer = 0;
 	
-	public static void push(Edit edit) {
-		while(stackPointer < edits.size()) {
-			edits.removeLast();
-		}
-		edits.addLast(edit);
-		stackPointer++;
+	static {
+		Thread newEditParser = new Thread(() -> {
+			try {
+				while(true) {
+					Edit edit = newEdits.pollFirst(Long.MAX_VALUE, TimeUnit.DAYS);
+					synchronized(edits) {
+						while(stackPointer < edits.size()) {
+							edits.removeLast();
+						}
+						edits.addLast(edit);
+						stackPointer++;
+					}
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		newEditParser.start();
+	}
+	
+	public synchronized static void push(Edit edit) {
+		newEdits.addLast(edit);
 	}
 	public static void undo() {
-		if(stackPointer <= 0) {
-			return;
+		synchronized(edits) {
+			if(stackPointer <= 0) {
+				return;
+			}
+			stackPointer--;
+			Edit toapply = edits.get(stackPointer);
+			edits.set(stackPointer, toapply.getInverse());
+			toapply.apply();
 		}
-		stackPointer--;
-		Edit toapply = edits.get(stackPointer);
-		edits.set(stackPointer, toapply.getInverse());
-		toapply.apply();
 	}
 	public static void redo() {
-		if(stackPointer >= edits.size()) {
-			return;
+		synchronized(edits) {
+			if(stackPointer >= edits.size()) {
+				return;
+			}
+			Edit toapply = edits.get(stackPointer);
+			edits.set(stackPointer, toapply.getInverse());
+			toapply.apply();
+			stackPointer++;
 		}
-		Edit toapply = edits.get(stackPointer);
-		edits.set(stackPointer, toapply.getInverse());
-		toapply.apply();
-		stackPointer++;
 	}
 }
